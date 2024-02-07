8f010686c0443d6904cca29ab1a6a1b1f0514fa2
0xlakshay <128704670+0xlakshay@users.noreply.github.com>
2024-02-08 00:08:26 +0530

    feat: add command alias

--- hector/cli.py
+++ hector/cli.py
@@ -1,6 +1,9 @@
 import typer
 
+from typing_extensions import Annotated
+
 from .typers import bot_typer, diff_typer, report_typer
+from .typers.bot import post
 
 
 core_typer = typer.Typer(
@@ -9,6 +12,20 @@ core_typer = typer.Typer(
 core_typer.add_typer(diff_typer, name="diff")
 core_typer.add_typer(bot_typer, name="bot")
 core_typer.add_typer(report_typer, name="report")
+
+
+@core_typer.command(help="Post a comment. Alias for `bot post`")
+def comment(
+    bitbucket: Annotated[
+        bool,
+        typer.Option(help="Post comment to bitbucket PR."),
+    ] = False,
+    dry: Annotated[
+        bool,
+        typer.Option(help="Only prepare comment without actually posting."),
+    ] = False,
+):
+    post(bitbucket=bitbucket, dry=dry)
 
 
 if __name__ == "__main__":
